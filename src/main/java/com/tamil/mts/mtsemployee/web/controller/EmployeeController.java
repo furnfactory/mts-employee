128b16257fed4a332e2ac7c72ef45ddc7120daf8
Murugan Nagarajan <murugan425@gmail.com>
2020-10-08 20:49:03 +0530

    Global Exception Handling - using @ControllerAdvice

--- src/main/java/com/tamil/mts/mtsemployee/web/controller/EmployeeController.java
+++ src/main/java/com/tamil/mts/mtsemployee/web/controller/EmployeeController.java
@@ -5,21 +5,16 @@
  */
 package com.tamil.mts.mtsemployee.web.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
-import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import org.hibernate.exception.spi.ViolatedConstraintNameExtracter;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,20 +27,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tamil.mts.mtsemployee.services.EmployeeService;
 import com.tamil.mts.mtsemployee.web.model.EmployeeDto;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * @author murugan
  *
  */
-@Slf4j
 @Validated
 @RequestMapping("api/v1/employee")
 @RestController
 public class EmployeeController {
 
-	private Object object;
-	private Object object2;
 	private final EmployeeService employeeService;
 
 	public EmployeeController(EmployeeService employeeService) {
@@ -79,27 +69,4 @@ public class EmployeeController {
 	public void deleteEmployee(@NotNull @PathVariable("empId") UUID empId) {
 		employeeService.deleteEmployeeById(empId);
 	}
-
-	// TODO - Difference between ConstraintViolation & MethodArgumentNotValid
-	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<List<String>> validationErrorHandler(ConstraintViolationException ex) {
-		List<String> validationErrors = new ArrayList<>(ex.getConstraintViolations().size());
-		ex.getConstraintViolations().forEach(violation -> {
-			validationErrors.add(violation.getPropertyPath() + "_" + violation.getMessage());
-		});
-		return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
-	}
-
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<List<String>> validationErrorHandler(MethodArgumentNotValidException ex) {
-		List<String> validationErrors = new ArrayList<>(ex.getBindingResult().getErrorCount());
-		ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
-			log.warn("Dto: {}, Invalid Request: {} = {},  Message: {}", fieldError.getObjectName(),
-					fieldError.getField(), fieldError.getRejectedValue(), fieldError.getDefaultMessage());
-			validationErrors.add(String.format("%s = %s, message: %s", fieldError.getField(),
-					fieldError.getRejectedValue(), fieldError.getDefaultMessage()));
-		});
-		return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
-	}
-
 }
