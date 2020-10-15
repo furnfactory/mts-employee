06bee029c842adb228def92a2210997d245f1e24
Murugan Nagarajan <murugan425@gmail.com>
2020-10-15 23:39:32 +0530

    Added constraint of the model fields in the request fields snippet generated by spring restdocs

--- src/test/java/com/tamil/mts/mtsemployee/web/controller/EmployeeControllerTest.java
+++ src/test/java/com/tamil/mts/mtsemployee/web/controller/EmployeeControllerTest.java
@@ -15,7 +15,8 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.requestF
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.math.BigDecimal;
@@ -30,7 +31,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tamil.mts.mtsemployee.services.EmployeeService;
@@ -60,20 +64,24 @@ public class EmployeeControllerTest {
 	@Test
 	public void getEmployeeById() throws Exception {
 		given(employeeService.getEmployeeById(any(UUID.class))).willReturn(getValidEmployeeDto());
-		mockMvc.perform(
-				get(EMPLOYEE_API_PATH + "{empId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
+
+		ConstrainedFields fields = new ConstrainedFields(EmployeeDto.class);
+
+		mockMvc.perform(get(EMPLOYEE_API_PATH + "{empId}", UUID.randomUUID().toString()).param("alldetails", "yes")
+				.accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andDo(document("api/v1/employee/",
 						pathParameters(parameterWithName("empId").description("UUID of the desired Employee Id.")),
-						responseFields(fieldWithPath("id").description("Employee UUID Id"),
-								fieldWithPath("version").description("Version number"),
-								fieldWithPath("name").description("Employee Name"),
-								fieldWithPath("age").description("Employee Age"),
-								fieldWithPath("createdDate").description("Created Date"),
-								fieldWithPath("lastModifiedDate").description("Date Updated"),
-								fieldWithPath("joiningDate").description("Employee Joining Date"),
-								fieldWithPath("employeeType").description("Employee Type"),
-								fieldWithPath("salary").description("Base Salary"))));
+						requestParameters(
+								parameterWithName("alldetails").description("Get all details of the Employee.")),
+						responseFields(fields.withPath("id").description("Employee UUID Id"),
+								fields.withPath("version").description("Version number"),
+								fields.withPath("name").description("Employee Name"),
+								fields.withPath("age").description("Employee Age"),
+								fields.withPath("createdDate").description("Created Date"),
+								fields.withPath("lastModifiedDate").description("Date Updated"),
+								fields.withPath("joiningDate").description("Employee Joining Date"),
+								fields.withPath("employeeType").description("Employee Type"),
+								fields.withPath("salary").description("Base Salary"))));
 	}
 
 	@Test
@@ -81,16 +89,19 @@ public class EmployeeControllerTest {
 		EmployeeDto employeeDto = getNewEmployeeDto();
 		String employeeDtoJson = objectMapper.writeValueAsString(employeeDto);
 		given(employeeService.saveNewEmployee(any(EmployeeDto.class))).willReturn(getValidEmployeeDto());
+
+		ConstrainedFields fields = new ConstrainedFields(EmployeeDto.class);
+
 		mockMvc.perform(post(EMPLOYEE_API_PATH).contentType(MediaType.APPLICATION_JSON).content(employeeDtoJson))
 				.andExpect(status().isCreated())
 				.andDo(document("api/v1/employee/",
-						requestFields(fieldWithPath("id").ignored(), fieldWithPath("version").ignored(),
-								fieldWithPath("createdDate").ignored(), fieldWithPath("lastModifiedDate").ignored(),
-								fieldWithPath("name").description("Employee Name"),
-								fieldWithPath("age").description("Employee Age"),
-								fieldWithPath("joiningDate").description("Employee Joining Date"),
-								fieldWithPath("employeeType").description("Employee Type"),
-								fieldWithPath("salary").description("Salary Amount [INR]"))));
+						requestFields(fields.withPath("id").ignored(), fieldWithPath("version").ignored(),
+								fields.withPath("createdDate").ignored(), fieldWithPath("lastModifiedDate").ignored(),
+								fields.withPath("name").description("Employee Name"),
+								fields.withPath("age").description("Employee Age"),
+								fields.withPath("joiningDate").description("Employee Joining Date"),
+								fields.withPath("employeeType").description("Employee Type"),
+								fields.withPath("salary").description("Salary Amount [INR]"))));
 	}
 
 	@Test
@@ -114,5 +125,19 @@ public class EmployeeControllerTest {
 
 	private EmployeeDto getInvalidEmployeeDto() {
 		return EmployeeDto.builder().id(UUID.randomUUID()).name("").age(90).employeeType(EmployeeType.LABOUR).build();
+	}
+
+	private static class ConstrainedFields {
+
+		private final ConstraintDescriptions constraintDescriptions;
+
+		ConstrainedFields(Class<?> input) {
+			this.constraintDescriptions = new ConstraintDescriptions(input);
+		}
+
+		private FieldDescriptor withPath(String path) {
+			return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+					.collectionToDelimitedString(this.constraintDescriptions.descriptionsForProperty(path), ". ")));
+		}
 	}
 }
